420de127e76be2b3740ea95b5029a0dbf2df3957
matt harrison <matt@mymailhost.co.uk>
2024-09-16 21:19:08 +0100

    slider fix

--- src/components/ratio-slider.tsx
+++ src/components/ratio-slider.tsx
@@ -7,15 +7,16 @@ export default function RatioSlider({
   minRatio: number;
   setMinRatio: React.Dispatch<React.SetStateAction<number>>;
 }) {
+  console.log(minRatio);
   return (
     <div>
       <input
         type="range"
         value={minRatio}
-        min={0}
-        max={21}
-        step={0.01}
-        onChange={(ev) => setMinRatio(parseInt(ev.target.value, 10))}
+        min="0"
+        max="21"
+        step="0.01"
+        onChange={(ev) => setMinRatio(Number(ev.target.value))}
       />
     </div>
   );
